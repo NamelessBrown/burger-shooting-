5ec935f2b44ba8f992b2960bce6234bd708f6f5b
Nameless <tyrebrown873@gmail.com>
2020-04-05 16:09:18 -0400

    Speed used in the movement

--- SFML_testing_collion/BadGuys.cpp
+++ SFML_testing_collion/BadGuys.cpp
@@ -7,7 +7,7 @@ void BadGuys::movement(float x, float y)
 }
 
 BadGuys::BadGuys()
-	:m_speed(2.5f), m_attackDamage(20), m_hp(100)
+	:m_speed(1.f)
 {
 	std::random_device rd;
 	std::mt19937 rng(rd());
@@ -18,10 +18,6 @@ BadGuys::BadGuys()
 	m_guys.setTexture(m_guyTexture);
 	m_guys.setScale(m_guys.getScale() / 14.f);
 	m_guys.setPosition(sf::Vector2f(distrubutionX(rng), distrubutionY(rng)));
-
-	m_laser.setFillColor(sf::Color::Red);
-	m_laser.setSize( m_laser.getSize() / 2.f);
-	m_laser.setPosition(m_guys.getPosition());
 }
 
 BadGuys::~BadGuys()
@@ -30,8 +26,7 @@ BadGuys::~BadGuys()
 
 void BadGuys::update(float x, float y, float deltaTime)
 {
-	movement(x * deltaTime, y * deltaTime);
-
+	movement(x * deltaTime + m_speed, y * deltaTime);
 }
 
 void BadGuys::render(sf::RenderTarget& target)
--- SFML_testing_collion/BadGuys.h
+++ SFML_testing_collion/BadGuys.h
@@ -13,14 +13,12 @@ public:
 
 	const sf::FloatRect getBounds() const { return m_guys.getGlobalBounds(); }
 	const sf::Sprite getBadGuy() const { return m_guys; }
+
 private:
 	float m_speed;
-	int m_attackDamage;
-	int m_hp;
 
 	sf::Texture m_guyTexture;
 	sf::Sprite m_guys;
-	sf::RectangleShape m_laser;
 
 	void movement(float x, float y);
 };
